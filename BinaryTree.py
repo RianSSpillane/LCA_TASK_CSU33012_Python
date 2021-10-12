6ab8131fa9a028b0806c16849bc734f49059b904
RianSSpillane <91212260+RianSSpillane@users.noreply.github.com>
2021-10-12 20:35:52 +0100

    Working LCA basic implementation

--- BinaryTree.py
+++ BinaryTree.py
@@ -1,18 +1,11 @@
 #File for implementing Bianry Tree and LCA method
-# class Node:
-#     def __init__(self, val):
-#         self.left= None
-#         self.right = None
-#         self.v = val
-            
+class Node:
+    def __init__(self, val):
+        self.left= None
+        self.right = None
+        self.v = val
 
 class Tree:
-
-    class Node:
-        def __init__(self, val):
-            self.left= None
-            self.right = None
-            self.v = val
             
     # Constructor
     def __init__(self):
@@ -70,34 +63,64 @@ class Tree:
             print(str(node.v) + ' ')
             self._printTree(node.right)
 
-
-
     
+def findPath( root, path, k):
+# Base Case
+    if root is None:
+        return False
 
- 
-# Driver program to test above function
-# Let's create the Binary Tree shown in above diagram
-# root = Node(1)
-# root.left = Node(2)
-# root.right = Node(3)
-# root.left.left = Node(4)
-# root.left.right = Node(5)
-# root.right.left = Node(6)
-# root.right.right = Node(7)
+    # Store this node is path vector. The node will be
+    # removed if not in path from root to k
+    path.append(root.v)
+
+    # See if the k is same as root's v
+    if root.v == k :
+        return True
+
+    # Check if k is found in left or right sub-tree
+    if ((root.left != None and findPath(root.left, path, k)) or
+            (root.right!= None and findPath(root.right, path, k))):
+        return True
+
+    # If not present in subtree rooted with root, remove
+    # root from path and return False
+    path.pop()
+    return False
 
 
- 
-# print "tree.LCA(4, 5) = %d" %(findLCA(root, 4, 5,))
+# Returns LCA if node n1 , n2 are present in the given
+# binary tre otherwise return -1
+def findLCA(root, n1, n2):
 
-# print "LCA(4, 6) = %d" %(findLCA(root, 4, 6))
-# print "LCA(3, 4) = %d" %(findLCA(root,3,4))
-# print "LCA(2, 4) = %d" %(findLCA(root,2, 4))
+    # To store paths to n1 and n2 fromthe root
+    path1 = []
+    path2 = []
 
+    # Find paths from root to n1 and root to n2.
+    # If either n1 or n2 is not present , return -1
+    if (not findPath(root, path1, n1) or not findPath(root, path2, n2)):
+        return -1
+
+    # Compare the paths to get the first different value
+    i = 0
+    while(i < len(path1) and i < len(path2)):
+        if path1[i] != path2[i]:
+            break
+        i += 1
+    return path1[i-1]
+
+
+
+
+
+#Check if it is working : 
 tree = Tree()
-tree.add(3)
+tree.add(6)
 tree.add(4)
-tree.add(0)
-tree.add(8)
+tree.add(10)
 tree.add(2)
-tree.printTree()
-#tree.findLCA(4,2)
+tree.add(5)
+tree.add(6)
+tree.add(3)
+#tree.printTree()
+print(findLCA(tree.root, 4, 10))
